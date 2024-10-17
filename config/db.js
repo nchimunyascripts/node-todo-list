b751ecd2fdaee2f8bae56addd250266692271eed
nchimunya-joseph <nchimunya.backlogcode@gmail.com>
2024-10-17 18:17:11 +0200

    nodemon & clear up

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
-const session = require('express-session');
 const RedisStore = require('connect-redis').default;
 
 // Create a Redis client and enable legacy mode for Redis v4+
@@ -9,20 +8,12 @@ const redisClient = redis.createClient({
     legacyMode: true,          
 });
 
-// Connect Redis client
 redisClient.connect().catch((err) => console.error('Redis connection error:', err));
 
-// Initialize MongoDB and Redis connections
 const connectDB = async () => {
     try {
-        // Connect to MongoDB
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected');
-
-        // Handle Redis connection status
         redisClient.on('ready', () => {
             console.log('Redis connected');
         });
@@ -30,14 +21,11 @@ const connectDB = async () => {
         redisClient.on('error', (err) => {
             console.error('Redis error:', err);
         });
-
-        // Return both the Redis client and Redis store
         const store = new RedisStore({ client: redisClient });
-
         return { redisClient, store };
     } catch (err) {
         console.error('Error connecting to the database:', err);
-        process.exit(1);  // Exit the process with failure code
+        process.exit(1);
     }
 };
 
